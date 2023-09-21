a9a8bc46849d400f88725db2fac9a4591d1bba0e
vilsonpneto <vilson@kenzie.com.br>
2023-09-21 10:31:34 -0300

    feat: Estilizado Header e BannerSection / Criado dados de produtos e categorias

--- src/components/sections/BannerSection/index.jsx
+++ src/components/sections/BannerSection/index.jsx
@@ -1,14 +1,17 @@
 import Banner from "../../../assets/banner.jpg"
+import styles from "./style.module.css"
 
 export const BannerSection = () => {
     return(
-        <section>
-            <img src={Banner} alt="imagem do Banner" />
-            <div>
-                <h1 className="title1">Fashion Store</h1>
-                <p className="paragraph">Fique por dentro das nossas últimas promoções e novidades</p>
-                <a href="#" className="btn">Confira as OFERTAS</a>
-            </div>
-        </section>
+        <div className="container">
+            <section className={styles.flexbox}>
+                <img src={Banner} alt="imagem do Banner" />
+                <div>
+                    <h1 className="title1">Fashion Store</h1>
+                    <p className="paragraph">Fique por dentro das nossas últimas promoções e novidades</p>
+                    <a href="#" className="btn">Confira as OFERTAS</a>
+                </div>
+            </section>
+        </div>
     )
 }
--- src/components/sections/CategoriesSection/index.jsx
+++ src/components/sections/CategoriesSection/index.jsx
@@ -1,17 +1,22 @@
-import { CategoryCard } from "./CategoryCard"
-import Category1 from "../../../assets/category1.png"
-import Category2 from "../../../assets/category2.png"
+import { CategoryCard } from "./CategoryCard";
+import Category1 from "../../../assets/category1.png";
+import Category2 from "../../../assets/category2.png";
 
 export const CategoriesSection = () => {
-
-    //categoryCard("Sapatos", "http://...")
-    return (
-        <section>
-            <h2 className="title2">CATEGORIAS</h2>
-            <ul>
-                <li><CategoryCard title="Sapatos" image={Category1}/></li>
-                <li><CategoryCard title="Bolsas" image={Category2}/></li>
-            </ul>
-        </section>
-    )
-}
+  //categoryCard("Sapatos", "http://...")
+  return (
+    <div className="container">
+      <section>
+        <h2 className="title2">CATEGORIAS</h2>
+        <ul>
+          <li>
+            <CategoryCard title="Sapatos" image={Category1} />
+          </li>
+          <li>
+            <CategoryCard title="Bolsas" image={Category2} />
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+};
